5b70a6d4d7e960725758dd6d291cab88b0d4027b
Krishna Das <120264734+krishnaswgithub@users.noreply.github.com>
2023-06-09 19:43:13 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,3 @@
-// import React ,{useRef,useState,useEffect} from 'react'
-// import '../styles/App.css';
-// import InputField from './InputField.js';
-// const App = () => {
- 
-// //code here 
-
-
- 
-//   return (
-//     <div>
-//     <InputField  ref={newRef}  type="text"/><br/>
-//     <button id="settingValueButton" onClick={settingValue}>Set Value</button>
-//     <button id="focusInputButton" onClick={focusInput}>Focus the input</button><br/><br/>
-//      <textarea id="textarea" value={values}></textarea>
-
-//     </div>
-//   );
-// }
-
-
-// export default App;
-
-
 import React, { useRef, useState } from 'react';
 import '../styles/App.css';
 import InputField from './InputField.js';
