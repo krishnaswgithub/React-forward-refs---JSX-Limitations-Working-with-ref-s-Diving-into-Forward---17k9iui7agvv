273803b7a34587f252e733a07b5077b8b4a093e4
Krishna Das <120264734+krishnaswgithub@users.noreply.github.com>
2023-06-09 19:05:17 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,52 @@
-import React ,{useRef,useState,useEffect} from 'react'
+// import React ,{useRef,useState,useEffect} from 'react'
+// import '../styles/App.css';
+// import InputField from './InputField.js';
+// const App = () => {
+ 
+// //code here 
+
+
+ 
+//   return (
+//     <div>
+//     <InputField  ref={newRef}  type="text"/><br/>
+//     <button id="settingValueButton" onClick={settingValue}>Set Value</button>
+//     <button id="focusInputButton" onClick={focusInput}>Focus the input</button><br/><br/>
+//      <textarea id="textarea" value={values}></textarea>
+
+//     </div>
+//   );
+// }
+
+
+// export default App;
+
+
+import React, { useRef, useState } from 'react';
 import '../styles/App.css';
 import InputField from './InputField.js';
+
 const App = () => {
- 
-//code here 
+  const textareaRef = useRef(null);
+  const inputRef = useRef(null);
+  const [values, setValues] = useState('');
 
+  const settingValue = () => {
+    setValues(inputRef.current.value);
+  };
 
- 
+  const focusInput = () => {
+    inputRef.current.focus();
+  };
+
   return (
     <div>
-    <InputField  ref={newRef}  type="text"/><br/>
-    <button id="settingValueButton" onClick={settingValue}>Set Value</button>
-    <button id="focusInputButton" onClick={focusInput}>Focus the input</button><br/><br/>
-     <textarea id="textarea" value={values}></textarea>
-
+      <InputField ref={inputRef} type="text" /><br/>
+      <button id="settingValueButton" onClick={settingValue}>Set Value</button>
+      <button id="focusInputButton" onClick={focusInput}>Focus the input</button><br/><br/>
+      <textarea id="textarea" ref={textareaRef} value={values}></textarea>
     </div>
   );
 }
-
 
 export default App;
