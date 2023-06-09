2b4b55b2cc96c9508af8c8312b33a3ef0e9e0e82
Krishna Das <120264734+krishnaswgithub@users.noreply.github.com>
2023-06-09 19:43:42 +0530

    Update InputField.js

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -1,17 +1,3 @@
-// import React ,{useRef,useState,useEffect} from 'react';
-// import '../styles/App.css';
-
-// //complete the following using forward refs concept 
-
-// const InputField=()=>{
-//     return(
-//     <div>
-//       <input id="input" type="text"  ref={ref}/>
-//     </div>
-//     )
-//     }
-// export default InputField;
-
 import React from 'react';
 
 const InputField = React.forwardRef((props, ref) => {
